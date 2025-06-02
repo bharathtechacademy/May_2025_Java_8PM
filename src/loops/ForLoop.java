fc4207ccb927d20c0ddbf28622bd62d5cfffd38a
bharathautomation <soft.ittech20@gmail.com>
2025-06-02 21:32:47 +0530

    Switch Case & Loops

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,22 @@
+package loops;
+
+public class ForLoop {
+	
+//	for(condition1 ; condition2;condition3) {
+//		//code to repeat
+//	}
+	
+//	condition1 ==> where to start the loop
+//	condition2 ==> where to end the loop
+//	condition3 ==> interval between each iteration
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10 ; i++) {
+			System.out.println(i+"."+"Akash");
+		}
+		
+
+	}
+
+}
