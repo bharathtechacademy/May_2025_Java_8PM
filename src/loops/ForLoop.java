00396dd571f083a547e4242f6e04d50aa31fb5be
bharathautomation <soft.ittech20@gmail.com>
2025-06-05 21:26:47 +0530

    Methods in Java

--- src/loops/ForLoop.java
+++ src/loops/ForLoop.java
@@ -13,7 +13,11 @@ public class ForLoop {
 	public static void main(String[] args) {
 		
 		for(int i=1; i<=10 ; i++) {
-			System.out.println(i+"."+"Akash");
+			System.out.println(i);
+			
+			for(int j=1; j<=10; j++) {
+				System.out.println(i+""+j);				
+			}
 		}
 		
 
